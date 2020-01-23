b72d157a920af18d1fbd2508800623777c2b7fd9
Karolina Pieszczek <karolina.pieszczekk@gmail.com>
2020-01-23 18:50:05 +0100

    hibernate

---
+++ Hibernate/Hibernate/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hibernate
+{
+    class Car
+    {
+        public virtual int CarId { get; set; }
+        public virtual string Brand { get; set; }
+        public virtual string Model { get; set; }
+    }
+
+}
